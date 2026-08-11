7e7903d25a650f19a374f08d427998df440076a3
shubh mishra <smishra64310@gmail.com>
2026-08-11 19:48:13 +0530

    completed the service part of the dashboard repository

--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -1,5 +1,30 @@
+import { dashboardRepository } from "@/repositories/dashboard.repository";
+
 class DasboardService {
-  async dashboardDetails(userId) {}
+  async dashboardDetails(userId) {
+    const totalLentamount =
+      await dashboardRepository.getTotalLentAmount(userId);
+    const totalPendingAmount =
+      await dashboardRepository.getPendingAmount(userId);
+    const todayDueamount = await dashboardRepository.getTodayDueAmount(userId);
+    const overdueamount = await dashboardRepository.getOverdueAmount(userId);
+
+    const activeBorrowers =
+      await dashboardRepository.getActiveBorrowersCount(userId);
+    const todayborrowers =
+      await dashboardRepository.getTodayDueBorrowerscount(userId);
+    const overdueBorrowers =
+      await dashboardRepository.getOverdueBorrowers(userId);
+    return {
+      totalLentamount,
+      totalPendingAmount,
+      todayDueamount,
+      overdueamount,
+      activeBorrowers,
+      todayborrowers,
+      overdueBorrowers,
+    };
+  }
 }
 
 export const dashboardService = new DasboardService();
