d0e7d3924ebed5cd4ff5d687d5d53f012c7757b5
shubh mishra <smishra64310@gmail.com>
2026-08-13 12:47:40 +0530

    completed the repositry part of the recentPayments api

--- src/repositories/loan.Payments.repository.js
+++ src/repositories/loan.Payments.repository.js
@@ -1,7 +1,33 @@
+import prisma from "@/lib/prisma";
+
 class LoanPaymentRepository {
   createPayment(tx, paymentData) {
     return tx.loanPayment.create({
       data: paymentData,
+    });
+  }
+
+  recentPayments(userId) {
+    return prisma.loanPayment.findMany({
+      where: {
+        userId: userId,
+      },
+      include: {
+        loan: {
+          include: {
+            borrower: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        paymentDate: "desc",
+      },
+      take: 5,
     });
   }
 }
