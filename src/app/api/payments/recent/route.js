031226f00b4b83472d3781ac737f309f1d39126e
shubh mishra <smishra64310@gmail.com>
2026-08-13 11:47:49 +0530

    completed the route part of recent payments and now heading to the service part of the recentpayments api

---
+++ src/app/api/payments/recent/route.js
@@ -0,0 +1,54 @@
+import { AppError } from "@/errors/app-error";
+import { verifyToken } from "@/lib/auth";
+import { authService } from "@/services/auth.service";
+import { loanPaymentService } from "@/services/loan.Payments.service";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const cookiesStore = await cookies();
+    const token = cookiesStore.get("token")?.value;
+    if (!token) {
+      return NextResponse.json(
+        {
+          message: "unauthorized",
+        },
+        { status: 401 },
+      );
+    }
+
+    const decodePayload = await verifyToken(token);
+    const userId = decodePayload.id;
+    const currentUser = await authService.getCurrentUser(userId);
+
+    const recentPay = await loanPaymentService.recentPayments(currentUser.id);
+
+    return NextResponse.json(
+      {
+        message: "recent Payments fetched successfully",
+        recentPay,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof AppError) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: {
+            code: error.code,
+            details: error.message,
+          },
+        },
+        { status: error.statusCode },
+      );
+    }
+    return NextResponse.json(
+      {
+        message: "internal server error",
+      },
+      { status: 500 },
+    );
+  }
+}
